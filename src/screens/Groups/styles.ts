79a1e5a00315818aed6a737f17c1170e1086608d
Lucas Godoy <lhskatista@gmail.com>
2023-04-24 15:33:44 -0300

    Adiconado navegação

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-export const Container = styled.View`
+
+export const Container = styled(SafeAreaView)`
 background-color: ${props => props.theme.COLORS.GRAY_600};
 flex: 1;
 padding: 24px;
--- src/screens/Players/styled.ts
+++ src/screens/Players/styled.ts
@@ -1,15 +1,15 @@
 import styled, { css } from 'styled-components/native';
-
-export const Container = styled.View`
+import { SafeAreaView } from 'react-native-safe-area-context';
+export const Container = styled(SafeAreaView)`
 flex: 1;
-background-color: ${({theme}) => theme.COLORS.GRAY_600};
+background-color: ${({ theme }) => theme.COLORS.GRAY_600};
 
 padding: 24px;
 `
 
 export const Form = styled.View`
 width: 100%;
-background-color: ${({theme}) => theme.COLORS.GRAY_700};
+background-color: ${({ theme }) => theme.COLORS.GRAY_700};
 flex-direction: row;
 justify-content: center;
 border-radius: 6px;
@@ -23,7 +23,7 @@ margin: 32px 0 12px;
 `
 
 export const NumbersOfPlayers = styled.Text`
-${({theme}) => css`
+${({ theme }) => css`
 color: ${theme.COLORS.GRAY_200};
 font-family: ${theme.FONT_FAMILY.BOLD};
 font-size: ${theme.FONT_SIZE.SM}px;
